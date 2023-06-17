c82daad703cde193ffce25764fc56701626e503d
Jaspal99 <Khalsajaspalsingh6@gmail.com>
2023-06-17 14:10:49 +0530

    firebase included

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDllXZDGAcdHG2avU3DWJnjUnCFaybC-jY",
+    authDomain: "crypto-trek.firebaseapp.com",
+    projectId: "crypto-trek",
+    storageBucket: "crypto-trek.appspot.com",
+    messagingSenderId: "933051918303",
+    appId: "1:933051918303:web:a4f58fee9cf553abb2c66a"
+};
+
+export default firebaseConfig;
